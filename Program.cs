14d6d164dde3f3f8ff577f04dcbc6f28e0b49065
Leonardo Saraiva <leonardosaraiva1911@gmail.com>
2022-11-14 12:20:23 -0300

    Function: Thread e Sleep

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        static void Start()
+        {
+            int time = 10;
+            int currenTime = 0;
+
+            while(currenTime != time)
+            {
+                Console.Clear();
+                currenTime++;
+                Console.WriteLine(currenTime);
+                Thread.Sleep(1000);
+            }          
+        }
+    }
+}
